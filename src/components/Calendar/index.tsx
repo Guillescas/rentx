ea31fc1ecebfc4ddf0247ddfede33f71747f8a31
Guilherme Illescas <gui.illescas@gmail.com>
2021-06-14 18:54:10 -0300

    Finish calendar component

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,86 @@
+import React, { ReactElement } from 'react';
+import { Feather } from '@expo/vector-icons';
+
+import { useTheme } from 'styled-components';
+
+import {
+  Calendar as ReactCalendar,
+  LocaleConfig,
+} from 'react-native-calendars';
+
+LocaleConfig.locales['pt-br'] = {
+  monthNames: [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ],
+  monthNamesShort: [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez',
+  ],
+  dayNames: [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
+  ],
+  dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB'],
+  today: 'Hoje',
+};
+LocaleConfig.defaultLocale = 'pt-br';
+
+export const Calendar = (): ReactElement => {
+  const theme = useTheme();
+
+  return (
+    <ReactCalendar
+      renderArrow={direction => (
+        <Feather
+          size={24}
+          color={theme.colors.text}
+          name={direction === 'left' ? 'chevron-left' : 'chevron-right'}
+        />
+      )}
+      headerStyle={{
+        borderBottomWidth: 0.5,
+        borderBottomColor: theme.colors.text_details,
+        paddingBottom: 10,
+        marginBottom: 10,
+      }}
+      theme={{
+        textDayFontFamily: theme.fonts.primary_400,
+        textDayHeaderFontFamily: theme.fonts.secondary_500,
+        textDayHeaderFontSize: 10,
+        textMonthFontFamily: theme.fonts.secondary_600,
+        textMonthFontSize: 20,
+        monthTextColor: theme.colors.title,
+        arrowStyle: {
+          marginHorizontal: -15,
+        },
+      }}
+      minDate={new Date()}
+    />
+  );
+};
