45c6d28dd5e8b1f96c3511f512f6e31ee061b534
Guilherme Illescas <gui.illescas@gmail.com>
2021-06-15 23:58:41 -0300

    Get car detail from api

--- src/Screens/CarDetails/index.tsx
+++ src/Screens/CarDetails/index.tsx
@@ -1,22 +1,24 @@
 import React, { ReactElement } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { BackButton } from '../../components/BackButton';
 import { CarAccessory } from '../../components/CarAccessory';
 import { ImageSlider } from '../../components/ImageSlider';
 import { Button } from '../../components/Button';
 
-import SpeedSvg from '../../assets/speed.svg';
-import AccelerationSvg from '../../assets/acceleration.svg';
-import ForceSvg from '../../assets/force.svg';
-import GasolineSvg from '../../assets/gasoline.svg';
-import ExchangeSvg from '../../assets/exchange.svg';
-import PeopleSvg from '../../assets/people.svg';
-
 import * as S from './styles';
+
+import { ICarDTO } from '../../dtos/CarDTO';
+import getAccessoryIcon from '../../utils/getAccessoryIcon';
+
+interface ICarDetailsParams {
+  car: ICarDTO;
+}
 
 export const CarDetails = (): ReactElement => {
   const navigation = useNavigation();
+  const route = useRoute();
+  const { car } = route.params as ICarDetailsParams;
 
   return (
     <S.Container>
@@ -25,44 +27,39 @@ export const CarDetails = (): ReactElement => {
       </S.Header>
 
       <S.CardImages>
-        <ImageSlider
-          imagesUrl={[
-            'https://i.pinimg.com/originals/9a/eb/34/9aeb34414cf5a43cc58f0fe505c911f8.png',
-          ]}
-        />
+        <ImageSlider imagesUrl={car.photos} />
       </S.CardImages>
 
       <S.Content>
         <S.Details>
           <S.Description>
-            <S.Brand>Lamborghini</S.Brand>
-            <S.Name>Huracan</S.Name>
+            <S.Brand>{car.brand}</S.Brand>
+            <S.Name>{car.name}</S.Name>
           </S.Description>
 
           <S.Rent>
-            <S.Period>Ao dia</S.Period>
-            <S.Price>500</S.Price>
+            <S.Period>{car.rent.period}</S.Period>
+            <S.Price>{`R$ ${car.rent.price}`}</S.Price>
           </S.Rent>
         </S.Details>
 
         <S.CarAccessories>
-          <CarAccessory name="380km/h" icon={SpeedSvg} />
-          <CarAccessory name="3.2s" icon={AccelerationSvg} />
-          <CarAccessory name="800 HP" icon={ForceSvg} />
-          <CarAccessory name="380km/h" icon={GasolineSvg} />
-          <CarAccessory name="380km/h" icon={ExchangeSvg} />
-          <CarAccessory name="380km/h" icon={PeopleSvg} />
+          {car.accessories.map(accessory => (
+            <CarAccessory
+              key={accessory.type}
+              name={accessory.name}
+              icon={getAccessoryIcon(accessory.type)}
+            />
+          ))}
         </S.CarAccessories>
 
-        <S.About>
-          Asuyagsauiys aisyuga siuaygs iausyg aiusyag siauygs aiuysg
-        </S.About>
+        <S.About>{car.about}</S.About>
       </S.Content>
 
       <S.Footer>
         <Button
           title="Escolher período do aluguel"
-          onPress={() => navigation.navigate('Scheduling')}
+          onPress={() => navigation.navigate('Scheduling', { car })}
         />
       </S.Footer>
     </S.Container>
